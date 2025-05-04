072c1949fc353112744ce2d7f9384e63f786abf3
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-05-04 16:23:49 +0300

    Add test context

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -2,8 +2,12 @@ package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import ru.practicum.shareit.system.ShareItApp;
 
 @SpringBootTest
+@ContextConfiguration(classes = { ShareItApp.class })
 class ShareItTests {
 
 	@Test

4328d9fffec73678ea532d9b72f391ceebc167fa
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-05-05 10:37:26 +0300

    Add comments tests

--- src/test/java/ru/practicum/shareit/comment/controller/CommentControllerTest.java
+++ src/test/java/ru/practicum/shareit/comment/controller/CommentControllerTest.java
@@ -2,42 +2,19 @@ package ru.practicum.shareit.comment.controller;
 
 import ru.practicum.shareit.comment.dto.CommentDto;
 import ru.practicum.shareit.comment.interfaces.CommentService;
+import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.interfaces.ItemService;
-import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.system.config.ShareItApp;
+import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.interfaces.UserService;
-import ru.practicum.shareit.user.model.User;
-
-import com.sun.jdi.InternalException;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+import java.util.List;
 import java.util.Random;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes = ShareItApp.class)
 class CommentControllerTest {
@@ -53,23 +30,52 @@ class CommentControllerTest {
     }
 
     @Test
-    void addNewComment() {
-        User user = new User();
-        user.setEmail(genEmail());
-        user.setName("username");
+    void add() {
+        UserDto userDto = new UserDto();
+        userDto.setEmail(genEmail());
+        userDto.setName("username");
+        UserDto userSaved = userService.save(userDto);
 
-        Item item = new Item();
-        item.setDescription("Item descritption");
-        item.setOwner(user);
+        ItemDto itemDto = new ItemDto();
+        itemDto.setAvailable(true);
+        itemDto.setName("Item name");
+        itemDto.setDescription("Item descritption");
+        itemDto.setOwner(userSaved);
+        ItemDto itemSaved = itemService.add(itemDto, userSaved.getId());
 
         CommentDto commentDto = new CommentDto();
-        commentDto.setAuthorId(user.getId());
-        commentDto.setItemId(item.getId());
+        commentDto.setAuthorId(userSaved.getId());
+        commentDto.setItemId(itemSaved.getId());
         commentDto.setText("Comment text");
+
+        CommentDto commentSaved = commentService.add(commentDto);
+        List<CommentDto> comments = commentService.getAll();
+
+        assertTrue(comments.contains(commentSaved));
     }
 
     @Test
-    void deleteComment() {
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void findByItemId() {
+    }
+
+    @Test
+    void findByAuthorId() {
     }
 
     private String genEmail() {
